812e1d18b42e5c63b6f1f96dea65d32e8517315d
Jose <jvega4849@gmail.com>
2018-07-17 23:14:48 -0700

    path fixed

--- saleor/saleor/urls.py
+++ saleor/saleor/urls.py
@@ -53,12 +53,14 @@ translatable_urlpatterns = [
 urlpatterns = non_translatable_urlpatterns + i18n_patterns(
     *translatable_urlpatterns)
 
+# url(r'^static/(?P<path>.*)$', serve)] + static(
+
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
         # static files (images, css, javascript, etc.)
-        url(r'^static/(?P<path>.*)$', serve)] + static(
+        url(r'^static/(?P.*)$', serve)] + static(
             settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.ENABLE_SILK:
